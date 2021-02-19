494811d4b20ec8a0798ed58fbcbce23a30929f30
Mark Chang <ak0922747401@gmail.com>
2021-02-19 22:17:50 +0800

    update set-up-cluster.sh

--- script/set-up-cluster.sh
+++ script/set-up-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -ex
-gcloud container clusters create mark-k8s --num-nodes=3
-echo 'hello world!'
+gcloud container clusters create mark-k8s --num-nodes=3 --machine-type=custom-4-8192
+echo 'create k8s cluster mark-k8s'
